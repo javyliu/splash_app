1a435733420d7370d71c262ece700e1e0af3316a
qmliu <qmliu@pearlinpalm.com>
2020-08-11 15:36:36 +0800

    add constant and size_config

---
+++ lib/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xffff7643);
+const primaryLightColor = Color(0xffffecdf);
+const primaryGradientColor = LinearGradient(begin: Alignment.topLeft, end: Alignment.bottomRight, colors: [
+  Color(0xffffa53e), Color(0xffff7643),
+]);
+const secondaryColor = Color(0xff070707);
+const textColor = Color(0xff757575);
+
+const animationDuration = Duration(milliseconds: 200);
